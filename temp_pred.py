b8d5a86637f576326b0449e5adfaf0e165b14440
FubuGold <124594648+FubuGold@users.noreply.github.com>
2023-03-14 21:05:16 +0700

    Add files via upload

---
+++ temp_pred.py
@@ -0,0 +1,9 @@
+import pickle as pk
+import sklearn as skl
+import streamlit as st
+f = open('model.pickle', 'rb')
+model = pk.load(f)
+number = st.number_input('Input Temperature')
+if st.button('Predict'):
+    st.success(model.predict(number))
+
